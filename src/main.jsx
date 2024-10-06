4490a699f8f281c26119402cfdf39934707724f5
sgcm14 <sgcm14@gmail.com>
2024-10-05 23:18:47 -0500

    add deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Faqs from "./Faqs";
 import App from "./App";
 import "./index.css";
 
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/faqs",
+    element: <Faqs />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <RouterProvider router={router} />
+    {/* Para deploy se cambia por createHashRouter */}
+    {/* <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/faqs" element={<Faqs />} />
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter> */}
   </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://sgcm14.github.io/0523C02-router-basics-app/", // Ruta del repositorio
+  // base:"/", // para local descomentar aqui y comentar arriba
   plugins: [react()],
 })
